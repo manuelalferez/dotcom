f81ea5d73e81a5ccb8117984004ad1c45b2ecc2f
manuelalferez <manuelalferezruiz@gmail.com>
2022-11-18 12:00:06 +0100

    fix: removed projects because url is down https://gh-pinned-repos.egoist.sh

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,6 @@ import { h } from "preact";
 import { tw } from "@twind";
 import About from "@/components/About.tsx";
 import { SocialLinks } from "@/components/SocialLinks.tsx";
-import Projects from "@/components/Projects.tsx";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Post, Project } from "@/types.d.tsx";
 import { walk } from "walk";
@@ -17,15 +16,8 @@ const POSTS_DIRECTORY = "posts/";
 
 export const handler: Handlers<Project | null> = {
   async GET(_, ctx) {
-    const jsonResponse = await fetch(
-      "https://gh-pinned-repos.egoist.sh/?username=manuelalferez"
-    );
     await loadContent(POSTS_DIRECTORY);
-    if (jsonResponse.status === 404) {
-      return ctx.render(null);
-    }
-    const projects: Project = await jsonResponse.json();
-    return ctx.render(projects);
+    return ctx.render();
   },
 };
 
@@ -42,8 +34,6 @@ export default function Home({ data, ...props }: PageProps<Project[] | null>) {
       </Head>
       <About />
       <SocialLinks class={tw`mt(5 md:7)`} />
-      <hr class={tw`w-5/6 mx-auto my-10`} />
-      <Projects data={data} {...props} />
       <hr class={tw`w-5/6 mx-auto my-10`} />
       <Posts posts={POSTS} />
       <Footer />
